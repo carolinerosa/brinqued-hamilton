ad112854b6b1f090feccd91d400b2f34d7443c1c
thaynanmoreira.nave <thaynanmoreira.nave@61fdf675-3a1e-fe81-a06b-97991296a4e4>
2013-09-19 01:27:01 +0000

    A loja aew vendendo os itens

--- trunk/Sobreviva/src/com/example/sobreviva/activity/Loja.java
+++ trunk/Sobreviva/src/com/example/sobreviva/activity/Loja.java
@@ -1,5 +1,6 @@
 package com.example.sobreviva.activity;
 
+import com.example.sobreviva.ConfigUsuario;
 import com.example.sobreviva.GerenciadorDeImagens;
 import com.example.sobreviva.R;
 import com.example.sobreviva.R.layout;
@@ -15,53 +16,93 @@ import android.view.WindowManager;
 import android.widget.Toast;
 
 public class Loja extends Activity {
-	
-	private MainActivity GerenteCenas; 
+
+	private MainActivity GerenteCenas;
+	private ConfigUsuario config;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) 
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// Sem Activity e em modo Fullcreen.
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-	    this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-	    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		this.getWindow().addFlags(
+				WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		setContentView(R.layout.activity_loja);
 		GerenteCenas = MainActivity.GetInstance();
+		config = ConfigUsuario.getInstance();
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) 
-	{
+	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.loja, menu);
 		return true;
 	}
-	
-	public void Space(View v)
-	{
-		GerenciadorDeImagens.getInstance().setImageBolinha("space.png");
-		GerenciadorDeImagens.getInstance().setImageBackground("spacebg.png");
-		Toast.makeText(MainActivity.GetInstance(), "Agora seu tema é SPACE", Toast.LENGTH_SHORT).show(); 
-		//GerenteCenas.switchActivity(this, LojaBolinha.class);
+
+	public void Space(View v) {
+		if (config.getMoedas() > 100) {
+			config.setMoedas(config.getMoedas() - 100);
+			GerenciadorDeImagens.getInstance().setImageBolinha("space.png");
+			GerenciadorDeImagens.getInstance()
+					.setImageBackground("spacebg.png");
+			Toast.makeText(MainActivity.GetInstance(),
+					"Você gastou 100 moedas. Você tem " + config.getMoedas(),
+					Toast.LENGTH_SHORT).show();
+			// GerenteCenas.switchActivity(this, LojaBolinha.class);
+		} else {
+			Toast.makeText(MainActivity.GetInstance(),
+					"Você não tem dinheiro suficiente", Toast.LENGTH_SHORT)
+					.show();
+			Toast.makeText(
+					MainActivity.GetInstance(),
+					"Esse tema custa 100 moedas, faltam " + config.getMoedas()
+							+ "moedas", Toast.LENGTH_SHORT).show();
+		}
 	}
-	
-	public void Simpsom(View v)
-	{
-		GerenciadorDeImagens.getInstance().setImageBolinha("simpsom.png");
-		GerenciadorDeImagens.getInstance().setImageBackground("simpsombg.png");
-		Toast.makeText(MainActivity.GetInstance(), "Agora seu tema é Homer e a Rosca", Toast.LENGTH_SHORT).show(); 
-		//GerenteCenas.switchActivity(this, LojaBolinha.class);
+
+	public void Simpsom(View v) {
+		if (config.getMoedas() >= 200) {
+			config.setMoedas(config.getMoedas() - 200);
+			GerenciadorDeImagens.getInstance().setImageBolinha("simpsom.png");
+			GerenciadorDeImagens.getInstance().setImageBackground(
+					"simpsombg.png");
+			Toast.makeText(MainActivity.GetInstance(),
+					"Você gastou 200 moedas. Você tem " + config.getMoedas(),
+					Toast.LENGTH_SHORT).show();
+			// GerenteCenas.switchActivity(this, LojaBolinha.class);
+		} else {
+			Toast.makeText(MainActivity.GetInstance(),
+					"Você não tem dinheiro suficiente", Toast.LENGTH_SHORT)
+					.show();
+			Toast.makeText(
+					MainActivity.GetInstance(),
+					"Esse tema custa 200 moedas, faltam " + config.getMoedas()
+							+ "moedas", Toast.LENGTH_SHORT).show();
+		}
 	}
-	
-	public void MonstroSA(View v)
-	{
-		GerenciadorDeImagens.getInstance().setImageBolinha("monstrosa.png");
-		GerenciadorDeImagens.getInstance().setImageBackground("monstrosabg.png");
-		Toast.makeText(MainActivity.GetInstance(), "Agora seu tema é Monstros S.A.", Toast.LENGTH_SHORT).show(); 
-		//GerenteCenas.switchActivity(this, LojaBolinha.class);
+
+	public void MonstroSA(View v) {
+		if (config.getMoedas() >= 300) {
+			config.setMoedas(config.getMoedas() - 300);
+			GerenciadorDeImagens.getInstance().setImageBolinha("monstrosa.png");
+			GerenciadorDeImagens.getInstance().setImageBackground(
+					"monstrosabg.png");
+			Toast.makeText(MainActivity.GetInstance(),
+					"Você gastou 300 moedas. Você tem " + config.getMoedas(),
+					Toast.LENGTH_SHORT).show();
+			// GerenteCenas.switchActivity(this, LojaBolinha.class);
+		} else {
+			Toast.makeText(MainActivity.GetInstance(),
+					"Você não tem dinheiro suficiente", Toast.LENGTH_SHORT)
+					.show();
+			Toast.makeText(
+					MainActivity.GetInstance(),
+					"Esse tema custa 300 moedas, faltam " + config.getMoedas()
+							+ "moedas", Toast.LENGTH_SHORT).show();
+		}
 	}
 
 }
